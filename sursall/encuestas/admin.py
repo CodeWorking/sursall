be9f2f567b9840ad48a539aae7e51157f2a06832
unknown <DGo@.(none)>
2012-10-06 10:40:58 -0500

    Modificacion admin.py

--- sursall/encuestas/admin.py
+++ sursall/encuestas/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 from models import *
 
 admin.site.register(Persona)
+admin.site.register(Prueba)
+admin.site.register(Modulo)
+admin.site.register(Seccion)
+admin.site.register(Pregunta)
+admin.site.register(Prueba_Contestada)
+admin.site.register(Respuesta)
+admin.site.register(Seleccion)
+
