763145e3650f6f91bed9b15ae2348c3e34234ad4
carlos david <adrenalina701@hotmail.com>
2012-10-08 17:16:06 -0500

    correccion de error en views. todavia hay errores

--- sursall/encuestas/views.py
+++ sursall/encuestas/views.py
@@ -1,6 +1,6 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
@@ -24,6 +24,7 @@ def nuevousuario(request):
     else:
         formulario = UserCreationForm()
     return render_to_response('nuevousuario.html',{'formulario':formulario}, context_instance=RequestContext(request))
+
 
 def ingresar(request):
     if not request.user.is_anonymous():
